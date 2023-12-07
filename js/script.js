d176ec9c8d35726eaa6b6fdf6aba69bfbb9a20f1
parizqi aminullah <parizqiaminullah@gmail.com>
2023-12-08 00:02:02 +0700

    done push

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function luas(){
+    let alas = document.getElementById('alas').value;
+    let tinggi = document.getElementById('tinggi').value;
+    let hasil = (alas*tinggi)*0.5;
+
+    console.log(hasil);
+
+    document.getElementById('hasil').value = hasil;
+}
+function keliling(){
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
+    let c = document.getElementById('c').value;
+    let keliling = a + b + c ;
+
+    console.log(keliling);
+    document.getElementById('keliling').value = keliling;
+}
